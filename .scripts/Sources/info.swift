5466fddf39c420c50a7d6aeb8b308112ef129d37
문성욱 <msw@escore.co.kr>
2025-03-13 08:59:27 +0900

    + PROJECT.BUILD_CONFIG

--- .scripts/Sources/info.swift
+++ .scripts/Sources/info.swift
@@ -3,4 +3,5 @@
 public struct PROJECT {
     public static let RN_VERSION = ":RN_VERSION:"
     public static let PLATFORM_HASH = ":PLATFORM_HASH:"
+    public static let BUILD_CONFIG = ":BUILD_CONFIG:"
 }
