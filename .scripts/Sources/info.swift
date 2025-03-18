96b43051396a441b54b5be8f069f20eb3f094fa4
문성욱 <msw@escore.co.kr>
2025-03-18 22:36:37 +0900

    React-Native 0.77.1
    Squashed commit of the following:
    
    commit 0ceb1611484e3f11eb267c20944c07d89aadc454
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 22:01:39 2025 +0900
    
        FIX:add_dependency.yml
    
    commit 5d3cd7c3577b040b4779188d4c24815ce2099da8
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 21:58:07 2025 +0900
    
        FIX: add_dependency.yml
    
    commit 58a9f6b76f957b964694cd122cbeb34198dcdaa4
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 21:09:40 2025 +0900
    
        fix: add_dependency.yml
    
    commit e881233638f82c8319ecff8f0eb58457eea30472
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 21:06:27 2025 +0900
    
        fix: add_dependency.yml
    
    commit a28dbe967f669c733a497bc2a4e3b0d06727be34
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 21:02:26 2025 +0900
    
        fix: add_dependency.yml
    
    commit 4d5287b963bdf896f7311d373d34613649d6a9bb
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 20:55:17 2025 +0900
    
        FIX: add_dependency.yml
    
    commit 6b85c066c03809f34afe97c659580aa5984bb8f0
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 20:53:04 2025 +0900
    
        FIX: add_dependency.yml
    
    commit b974b39ac773ddc2eba5e981a7070c58d660b8a8
    Author: 문성욱 <msw@escore.co.kr>
    Date:   Tue Mar 18 20:45:51 2025 +0900
    
        fix: add_dependency.yml
    
    commit 0dcdcadff3e819dea5b2d3e53ba46ab6ed60928b
    Author: Sung Wook Moon <mtjddnr@gmail.com>
    Date:   Tue Mar 18 20:41:44 2025 +0900
    
        Dev/1.0.0 → main (#5)
    
        * Rebase old main
    
        * M: deploy.¥ml
        - 직접 트리거 가능
        - package.json파일이 변경되었을때만
        - README.md 생성시 RCTThirdPartyComponentsProvider.mm파일 첨부
    
        * M: deploy.yml 경고 수정
    
        * + GITHUB ACTION: add_dependency.yml
    
        * fix: deploy.yml
    
        ---------
    
        Co-authored-by: 문성욱 <msw@escore.co.kr>

--- .scripts/Sources/info.swift
+++ .scripts/Sources/info.swift
@@ -2,4 +2,6 @@
 
 public struct PROJECT {
     public static let RN_VERSION = ":RN_VERSION:"
+    public static let PLATFORM_HASH = ":PLATFORM_HASH:"
+    public static let BUILD_CONFIG = ":BUILD_CONFIG:"
 }
