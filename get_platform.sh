c9a650615616605b412cdf259cf8bd11f65878f6
문성욱 <msw@escore.co.kr>
2025-05-15 00:03:14 +0900

    M: platform - 코어 버전 정보가 누락

--- get_platform.sh
+++ get_platform.sh
@@ -15,13 +15,16 @@ process.stdin.on("end", () => {
     const nativeDeps = Object.keys(config.dependencies || {}).map(name => {
         const pkgJson = JSON.parse(fs.readFileSync(path.resolve("node_modules", name, "package.json"), "utf8"));
         return { name, version: pkgJson.version };
-    }).sort((a, b) => a.name.localeCompare(b.name));
+    });
+    nativeDeps.push({ name: "react-native", version: config.reactNativeVersion });
+    nativeDeps.sort((a, b) => a.name.localeCompare(b.name));
+    console.error(nativeDeps);
 
-  const hash = crypto.createHash("sha256").update(JSON.stringify(nativeDeps)).digest("hex");
-  if (hash == "17a27507790fa3a70efc63238bf76eb9f2f040139a114fb7db604068fff336b1") {
-    console.log("7790a3ca64cc5e2f35a1555f06110b3a9758f8d0023aa022999d3fcb7cdf67c6");
-  } else {
-    console.log(hash);
-  }
+    const hash = crypto.createHash("sha256").update(JSON.stringify(nativeDeps)).digest("hex");
+    if (hash == "17a27507790fa3a70efc63238bf76eb9f2f040139a114fb7db604068fff336b1") {
+        console.log("7790a3ca64cc5e2f35a1555f06110b3a9758f8d0023aa022999d3fcb7cdf67c6");
+    } else {
+        console.log(hash);
+    }
 });
 ';
